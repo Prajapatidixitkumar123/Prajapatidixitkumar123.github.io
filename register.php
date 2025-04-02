dc12bfa516227ca693408b857f551ff7fe15322f
Prajapatidixitkumar123 <159777176+Prajapatidixitkumar123@users.noreply.github.com>
2025-04-02 17:16:28 +0530

    Update register.php

--- register.php
+++ register.php
@@ -4,6 +4,9 @@ header('Access-Control-Allow-Methods: POST');
 header('Access-Control-Allow-Headers: Content-Type');
 header('Content-Type: application/json');
 
+// Prevent any HTML output before JSON
+ob_clean();
+
 if ($_SERVER['REQUEST_METHOD'] === 'OPTIONS') {
     http_response_code(200);
     exit();
